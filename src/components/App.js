47e1dc1ad3af2530020ee2ae433d13c6fb83e374
gridmaster <AntlerHorse@Gmail.com>
2021-02-20 12:27:57 -0500

    add art for gigs

--- src/components/App.js
+++ src/components/App.js
@@ -20,12 +20,12 @@ class App extends React.Component {
     ];
     
     const gigList = [
-      { key: 1, link: "../images/benefit.jpg", event: "Big whopper dopper!", date: "2/22/2021" },
-      { key: 2, link: "../images/fire.jpg", event: "Amazing music!", date: "2/11/2021" },
-      { key: 3, link: "../images/sticky.jpg", event: "Woo-Hoo!!!", date: "2/18/2021" },
-      { key: 3, link: "../images/blindfaith.jpg", event: "Can't find my way!", date: "2/26/2021" },
-      { key: 3, link: "../images/Wheelsofilre.jpg", event: "I'm down at the Crossroads!", date: "3/5/2021" },
-      { key: 3, link: "../images/electriclady.jpg", event: "Voodoo Chile'", date: "3/10/2021" }
+      { key: 1, link: "../images/gig1.jpg", event: "A really big show!", date: "2/22/2021" },
+      { key: 2, link: "../images/gig2.jpg", event: "Amazing live music!", date: "2/11/2021" },
+      { key: 3, link: "../images/gig3.jpg", event: "All the best music!", date: "2/18/2021" },
+      { key: 3, link: "../images/gig4.jpg", event: "Get your tickets now!!", date: "2/26/2021" },
+      { key: 3, link: "../images/gig5.jpg", event: "Come down to the Crossroads!", date: "3/5/2021" },
+      { key: 3, link: "../images/gig6.jpg", event: "Live music all night long!", date: "3/10/2021" }
     ];
 
     console.log(process.env.PUBLIC_URL);
